24942973df960ef1873be0a1b70cda13fb565fc6
Pedro Carvalho <pedro.carvalho@hhsurge.com>
2020-04-04 20:32:57 +0100

    finish project

--- app/src/main/java/com/keezag/weatherdataapi/MainActivity.kt
+++ app/src/main/java/com/keezag/weatherdataapi/MainActivity.kt
@@ -2,11 +2,82 @@ package com.keezag.weatherdataapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.keezag.weatherdataapi.model.OpenWeatherResponse
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var cityEditText: EditText
+    private lateinit var countryEditText: EditText
+    private lateinit var cityValueTextView: TextView
+    private lateinit var weatherValueTextView: TextView
+    private lateinit var temperatureValueTextView: TextView
+    private lateinit var sensationValueTextView: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.cityEditText = findViewById(R.id.cityEditText)
+        this.countryEditText = findViewById(R.id.countryEditText)
+        this.cityValueTextView = findViewById(R.id.cityValueTextView)
+        this.weatherValueTextView = findViewById(R.id.weatherValueTextView)
+        this.temperatureValueTextView = findViewById(R.id.temperatureValueTextView)
+        this.sensationValueTextView = findViewById(R.id.sensationValueTextView)
+
+        val button: Button = findViewById(R.id.pesquisarButton)
+        button.setOnClickListener {
+            getWeather()
+        }
+
+
+    }
+
+    private fun getWeather() {
+
+        val url = "https://api.openweathermap.org/"
+        val apiKey = "0223c68b8d2690d19fed1df57956d456"
+
+        var sendCity = cityEditText.text.toString()
+        var sendCountry = countryEditText.text.toString()
+
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(url).addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(OpenWeatherInterface::class.java)
+        val call = service.getCurrentWeatherByCity("$sendCity,$sendCountry", "metric", apiKey)
+
+
+        call.enqueue(object : Callback<OpenWeatherResponse> {
+            override fun onFailure(call: Call<OpenWeatherResponse>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "ERROR!", Toast.LENGTH_LONG)
+            }
+
+            override fun onResponse(
+                call: Call<OpenWeatherResponse>,
+                response: Response<OpenWeatherResponse>
+            ) {
+                if (response?.code() == 200) {
+                    val responseWeather = response.body()!!
+                    cityValueTextView.text = responseWeather.name
+                    weatherValueTextView.text = "${responseWeather.weather[0].main}"
+                    temperatureValueTextView.text = "${responseWeather.main.temp}C"
+                    sensationValueTextView.text = "${responseWeather.main.feelsLike}C"
+                }
+
+            }
+
+        })
+
+
     }
 }
--- app/src/main/java/com/keezag/weatherdataapi/OpenWeatherInterface.kt
+++ app/src/main/java/com/keezag/weatherdataapi/OpenWeatherInterface.kt
@@ -8,6 +8,7 @@ import retrofit2.http.Query
 interface OpenWeatherInterface {
     @GET ("data/2.5/weather?")
     fun getCurrentWeatherByCity(@Query("q")cidade: String,
-                                @Query("appid") app_id: String,
-                                @Query("units") units: String = "metrics"): Call<OpenWeatherResponse>
+                                @Query("units") units: String,
+                                @Query("APPID") app_id: String
+                                ): Call<OpenWeatherResponse>
 }
